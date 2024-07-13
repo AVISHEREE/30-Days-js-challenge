6f50d0df92d1575fb3c6db45afb3c3fc5103d575
AVISHERE <akashvyas2006@gmail.com>
2024-07-13 15:58:18 +0530

    Day 1

---
+++ day-1/index.js
@@ -0,0 +1,34 @@
+var num1 = 64;
+console.log(num1);
+
+let str1 = "javascript challenge accepted";
+console.log(str1);
+
+const isAccepted = true;
+console.log(isAccepted);
+
+let value1 = true;
+let value2 = "hello coders";
+let value3 = 7;
+let value4 = {
+    name:'Akash Vyas',
+    username:'AVISHERE',
+    age:18
+};
+let value5 = ['javascript','c','reactJs','css and Tailwindcss','nodeJs and Express.....'];
+console.log(typeof(value1));
+console.log(typeof(value2));
+console.log(typeof(value3));
+console.log(typeof(value4));
+console.log(typeof(value5));
+
+console.log(`before reassign : ${value2}`);
+value2 = "hello World";
+console.log(`After reassign : ${value2}`);
+
+const str2 = 45;
+console.log(`before reassign : ${str2}`);
+str2 = 56;
+console.log(`After reassign : ${str2}`);
+
+
