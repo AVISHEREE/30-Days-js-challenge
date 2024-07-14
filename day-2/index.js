0283021568894ff6e8d1bcc7fdee7df8e9151870
AVISHERE <akashvyas2006@gmail.com>
2024-07-14 13:43:31 +0530

    Day 2

---
+++ day-2/index.js
@@ -0,0 +1,34 @@
+let num1 = 6;
+let num2 = 4;
+console.log(num1+num2);
+console.log(num1-num2);
+console.log(num1*num2);
+console.log(num1/num2);
+console.log(num1%num2);
+
+let num3 = 2;
+num3 += 4;
+console.log(`+= operator : ${num3}`);
+num3 -= 1;
+console.log(`-= operator : ${num3}`);
+
+let num4 = 6;
+let num5 = 10;
+console.log(`greater than operator :${num4>num5}`);
+console.log(`less than operator :${num4<num5}`);
+console.log(`greater than or equall to operator :${num4<=num5}`);
+console.log(`less than or equall to operator :${num4>=num5}`);
+console.log(`value are same or not :${num4==num5}`);
+console.log(`value and type are same or not :${num4===num5}`);
+
+
+let num6 = 4;
+let num7 = 7;
+console.log((num6>num7 && num6==num7));
+console.log((num6<num7 || num6==num7));
+console.log(!(num6<num7 || num6==num7));
+num7 = -7;
+console.log((num6>0?'positive':'negetive'));
+console.log((num7>0?'positive':'negetive'));
+
+
